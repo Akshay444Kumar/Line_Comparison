8053198cd1513485c22cdc276326b95d0d3c577b
Akshay444Kumar <aryakshay444@gmail.com>
2023-03-17 16:43:11 +0530

    ADDED: Code to check Equality of two Lines

--- src/com/linecomparison/LineComparison.java
+++ src/com/linecomparison/LineComparison.java
@@ -1,24 +1,40 @@
-    package com.linecomparison;
+package com.linecomparison;
 
-    import java.util.Scanner;
+import java.util.Scanner;
 
-    public class LineComparison {
-        public static void main(String[] args) {
-            System.out.println("Welcome to the Line Comparison Computation Program");
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter the co-ordinates of Line 1");
-            System.out.println("Enter the x1 co-ordinate of Line 1: ");
-            double x1 = sc.nextDouble();
-            System.out.println("Enter the y1 co-ordinate of Line 1: ");
-            double y1 = sc.nextDouble();
-            System.out.println("Enter the x2 co-ordinate of Line 1: ");
-            double x2 = sc.nextDouble();
-            System.out.println("Enter the y2 co-ordinate of Line 1: ");
-            double y2 = sc.nextDouble();
+public class LineComparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Line Comparison Computation Program");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the co-ordinates of Line 1");
+        System.out.println("Enter the x1 co-ordinate of Line 1: ");
+        double x1 = sc.nextDouble();
+        System.out.println("Enter the y1 co-ordinate of Line 1: ");
+        double y1 = sc.nextDouble();
+        System.out.println("Enter the x2 co-ordinate of Line 1: ");
+        double x2 = sc.nextDouble();
+        System.out.println("Enter the y2 co-ordinate of Line 1: ");
+        double y2 = sc.nextDouble();
 
-            sc.close();
+        System.out.println("Enter the co-ordinates of Line 2");
+        System.out.println("Enter the x1 co-ordinate of Line 2: ");
+        double m1 = sc.nextDouble();
+        System.out.println("Enter the y1 co-ordinate of Line 2: ");
+        double n1 = sc.nextDouble();
+        System.out.println("Enter the x2 co-ordinate of Line 2: ");
+        double m2 = sc.nextDouble();
+        System.out.println("Enter the y2 co-ordinate of Line 2: ");
+        double n2 = sc.nextDouble();
 
-            double length1 = Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2));
-            System.out.println("Length of the line is:" +length1);
+        sc.close();
+
+        double length1 = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+        double length2 = Math.sqrt(Math.pow((m2 - m1), 2) + Math.pow((n2 - n1), 2));
+
+        if (length1 == length2) {
+            System.out.println("The lengths of two lines are equal.");
+        } else {
+            System.out.println("The lengths of two lines are NOT equal.");
         }
     }
+}
