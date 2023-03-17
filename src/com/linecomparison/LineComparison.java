58970e259730819de87479e2902906b201c5a52c
Akshay444Kumar <aryakshay444@gmail.com>
2023-03-17 16:28:23 +0530

    ADDED: Welcome Message to the Line Comparison Computation Program

---
+++ src/com/linecomparison/LineComparison.java
@@ -0,0 +1,10 @@
+    package com.linecomparison;
+
+    import java.util.Scanner;
+
+    public class LineComparison {
+        public static void main(String[] args) {
+            System.out.println("Welcome to the Line Comparison Computation Program");
+
+        }
+    }
